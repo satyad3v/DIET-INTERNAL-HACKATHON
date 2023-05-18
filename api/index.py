ac1c70bfff4b25d3dadaa3c7c652b73dc9d6566e
Satyad3v <mrcool145@outlook.com>
2023-05-18 20:37:22 +0530

    thats all

--- api/index.py
+++ api/index.py
@@ -74,5 +74,5 @@ def download(ID):
 
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True,host='0.0.0.0',port=port)
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0',port=port)
